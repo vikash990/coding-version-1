d81a2ae9e1127c5b0787f59955f20a24eb1d4362
vikash990 <vikashadubey>
2020-01-05 02:44:36 +0530

    Updates

---
+++ precache-manifest.7ab24b03ec9016c3927e3f3e18ec0b99.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "b78b95f05d04ba85fdb6",
+    "url": "/coding-version-2/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "b78b95f05d04ba85fdb6",
+    "url": "/coding-version-2/static/js/main.b78b95f0.chunk.js"
+  },
+  {
+    "revision": "9cd328e01a759a281db5",
+    "url": "/coding-version-2/static/js/runtime~main.9cd328e0.js"
+  },
+  {
+    "revision": "990527e7654a42aba93e",
+    "url": "/coding-version-2/static/css/2.6f3ce391.chunk.css"
+  },
+  {
+    "revision": "990527e7654a42aba93e",
+    "url": "/coding-version-2/static/js/2.990527e7.chunk.js"
+  },
+  {
+    "revision": "c6e5296af434017bf2893b6023ee22c0",
+    "url": "/coding-version-2/static/media/html.c6e5296a.jpg"
+  },
+  {
+    "revision": "b1e3da5731984c33c44416a466c24c3a",
+    "url": "/coding-version-2/static/media/nodejs.b1e3da57.jpg"
+  },
+  {
+    "revision": "1ccfd165d95059845ed892ebbc7eef9f",
+    "url": "/coding-version-2/static/media/c.1ccfd165.jpg"
+  },
+  {
+    "revision": "86b68c4b2c79fd04b6400fa257127316",
+    "url": "/coding-version-2/static/media/coding1.86b68c4b.jpg"
+  },
+  {
+    "revision": "f8d2b48c31c23399a2a263c4c71b58be",
+    "url": "/coding-version-2/static/media/coding2.f8d2b48c.jpg"
+  },
+  {
+    "revision": "5858ab7cfee836c7bcdab55ca3ffb150",
+    "url": "/coding-version-2/static/media/coding3.5858ab7c.jpg"
+  },
+  {
+    "revision": "775e5ca0abdbb93183147e0cc3706886",
+    "url": "/coding-version-2/static/media/coding4.775e5ca0.jpg"
+  },
+  {
+    "revision": "7a7694728f3bf26e151c8c4cb2e83145",
+    "url": "/coding-version-2/static/media/vikash.7a769472.jpg"
+  },
+  {
+    "revision": "43f85edbf30f1ef858a95e973baf1518",
+    "url": "/coding-version-2/index.html"
+  }
+];
